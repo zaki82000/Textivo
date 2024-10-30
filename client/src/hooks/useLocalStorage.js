54a1c084905f78721a0cd1f1fcceb383ec98af71
zaki82000 <yabdelshafy0@gmail.com>
2024-10-31 01:18:33 +0300

    update scr file for Sidebar

--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -1,19 +1,22 @@
 import { useEffect, useState } from 'react'
 
-const PREFIX = 'Textivo'
+const PREFIX = 'whatsapp-clone-'
+
 export default function useLocalStorage(key, initialValue) {
+  const prefixedKey = PREFIX + key
+  const [value, setValue] = useState(() => {
+    const jsonValue = localStorage.getItem(prefixedKey)
+    if (jsonValue != null) return JSON.parse(jsonValue)
+    if (typeof initialValue === 'function') {
+      return initialValue()
+    } else {
+      return initialValue
+    }
+  })
 
-    const prefixedKey = PREFIX + key
-    const [value, setValue] = useState(() => {
-        const jsonValue = localStorage.getItem(prefixedKey)
-        if (jsonValue != null) return JSON.parse(jsonValue)
-        if (typeof initialValue === 'function') return initialValue()
-        return initialValue
-    })
-    
-    useEffect(() => {
-        localStorage.setItem(prefixedKey, JSON.stringify(value))
-    }, [prefixedKey, value])
+  useEffect(() => {
+    localStorage.setItem(prefixedKey, JSON.stringify(value))
+  }, [prefixedKey, value])
 
-    return [value, setValue]
+  return [value, setValue]
 }
