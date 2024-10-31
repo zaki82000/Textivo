4b844b2c059bcd1bc32216e10a8cd7b0690b12dd
zaki82000 <yabdelshafy0@gmail.com>
2024-10-31 23:51:19 +0200

    update project file and add new feature

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,67 +1,55 @@
-import React, {useState} from 'react';
-import { Tab, Nav, Button, Modal } from 'react-bootstrap';
-import Conversations from './Conversations';
-import Contacts from './Contacts';
-import NewContactModal from './NewContactModal';
-import NewConversationModal from './NewConversationModal';
+import React, { useState } from 'react'
+import { Tab, Nav, Button, Modal } from 'react-bootstrap'
+import Conversations from './Conversations'
+import Contacts from './Contacts'
+import NewContactModal from './NewContactModal'
+import NewConversationModal from './NewConversationModal'
 
-const CONVERSATIONS_KEY = "conversations";
-const CONTACTS_KEY = "contacts";
+const CONVERSATIONS_KEY = 'conversations'
+const CONTACTS_KEY = 'contacts'
 
 export default function Sidebar({ id }) {
   const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY)
   const [modalOpen, setModalOpen] = useState(false)
-  const conversationOpen = activeKey === CONVERSATIONS_KEY
-
+  const conversationsOpen = activeKey === CONVERSATIONS_KEY
+  
   function closeModal() {
     setModalOpen(false)
   }
 
   return (
-    <div style={{
-      width: '100vw',
-      height: '100vh',
-      border: '5px solid #007bff', // تحديد لون وسمك الحد
-      boxSizing: 'border-box', // لمنع تأثير الحد على الأبعاد
-      display: 'flex',
-      justifyContent: 'flex-start' // محاذاة المحتوى إلى اليسار
-    }}>
-    <div style={{ width: '250px' }} className='d-flex flex-column'>
+    <div style={{ width: '250px' }} className="d-flex flex-column">
       <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
         <Nav variant="tabs" className="justify-content-center">
           <Nav.Item>
-            <Nav.Link eventKey={CONVERSATIONS_KEY}>
-              Conversations
-            </Nav.Link>
+            <Nav.Link eventKey={CONVERSATIONS_KEY}>Conversations</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey={CONTACTS_KEY}>
-              Contacts
-            </Nav.Link>
+            <Nav.Link eventKey={CONTACTS_KEY}>Contacts</Nav.Link>
           </Nav.Item>
         </Nav>
         <Tab.Content className="border-right overflow-auto flex-grow-1">
           <Tab.Pane eventKey={CONVERSATIONS_KEY}>
-            <Conversations/>
+            <Conversations />
           </Tab.Pane>
           <Tab.Pane eventKey={CONTACTS_KEY}>
-            <Contacts/>
+            <Contacts />
           </Tab.Pane>
         </Tab.Content>
         <div className="p-2 border-top border-right small">
-          Your Id: <span className='text-muted'>{id}</span>
+          Your Id: <span className="text-muted">{id}</span>
         </div>
         <Button onClick={() => setModalOpen(true)} className="rounded-0">
-          NEW {conversationOpen ? 'conversation': 'Contact'}
+          New {conversationsOpen ? 'Conversation' : 'Contact'}
         </Button>
       </Tab.Container>
-      <Modal show={modalOpen } onHid={closeModal}>
-        {conversationOpen ?
-          <NewConversationModal closeModal={closeModal}/> : 
-          <NewContactModal closeModal={closeModal}/>
+
+      <Modal show={modalOpen} onHide={closeModal}>
+        {conversationsOpen ?
+          <NewConversationModal closeModal={closeModal} /> :
+          <NewContactModal closeModal={closeModal} />
         }
       </Modal>
     </div>
-    </div>
-  );
+  )
 }
