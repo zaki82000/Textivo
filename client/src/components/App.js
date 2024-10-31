d68505b032d87b00e575794274582095073fd2c1
zaki82000 <yabdelshafy0@gmail.com>
2024-10-31 23:51:19 +0200

    update project file and add new feature

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import Login from './Login';
+import React from 'react'
+import Login from './Login'
 import useLocalStorage from '../hooks/useLocalStorage';
-import Dashboard from './Dashboard';
-import { ContactsProvider } from '../contexts/ContactsProvider';
+import Dashboard from './Dashboard'
+import { ContactsProvider } from '../contexts/ContactsProvider'
+import { ConversationsProvider } from '../contexts/ConversationsProvider';
+import { SocketProvider } from '../contexts/SocketProvider';
 
 function App() {
   const [id, setId] = useLocalStorage('id')
 
   const dashboard = (
-    <ContactsProvider>
+    <SocketProvider id={id}>
       <ContactsProvider>
-      <Dashboard id={id} />
+        <ConversationsProvider id={id}>
+          <Dashboard id={id} />
+        </ConversationsProvider>
       </ContactsProvider>
-    </ContactsProvider>
+    </SocketProvider>
   )
 
   return (
     id ? dashboard : <Login onIdSubmit={setId} />
- 
   )
 }
 
--- client/src/contexts/ContactsProvider.js
+++ client/src/contexts/ContactsProvider.js
@@ -1,9 +1,9 @@
 import React, { useContext } from 'react'
-import useLocalStorage from '../hooks/useLocalStorage'
+import useLocalStorage from '../hooks/useLocalStorage';
 
 const ContactsContext = React.createContext()
 
-export function useContacts(){
+export function useContacts() {
   return useContext(ContactsContext)
 }
 
@@ -11,10 +11,11 @@ export function ContactsProvider({ children }) {
   const [contacts, setContacts] = useLocalStorage('contacts', [])
 
   function createContact(id, name) {
-    setContacts(prevContacts => { 
-      return [...prevContacts, {id ,name}]
+    setContacts(prevContacts => {
+      return [...prevContacts, { id, name }]
     })
   }
+
   return (
     <ContactsContext.Provider value={{ contacts, createContact }}>
       {children}
